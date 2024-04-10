c3c4a0157fd2aac071f2d42c1271d5220d5db987
Aluno <aluno@edu.senac.br>
2024-04-09 21:16:01 -0300

    09/04

---
+++ apiflask/models/poo/conta.py
@@ -0,0 +1,18 @@
+#a classe é um conjunto de objetos
+
+class Conta:
+    def__init__(self,id,descricao,valor):
+        self.id = id
+        self.descricao = descricao
+        self.valor = valor
+
+#O que uma classe tem?+
+"""
+objetos
+construtor
+atributos
+funções(métado)
+get
+set
+works(trabalhos)
+"""
---
+++ apiflask/models/poo/pessoa.py
@@ -0,0 +1,14 @@
+class Pessoa:
+    """
+    Na classe Pessoa defina três atributos
+
+    noone,idade,cpf=none
+    """
+    
+     def__init__(self,nome,idade,cpf=none):
+         self.nome = nome
+         self.idade = idade
+         self.cpf =cpf
+
+         
+    
